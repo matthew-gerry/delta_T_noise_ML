ebad7e7034683e4b4dba81012d63e532a3b4972b
Matthew Gerry <mgerry04@gmail.com>
2024-09-12 10:58:30 -0400

    Added more to explanatory comments

--- channel_analysis.py
+++ channel_analysis.py
@@ -77,34 +77,34 @@ for index, row in df2.iterrows():
         df2.at[index,'tau2'] = tau2
         df2.at[index,'x'] = x
 
-# Drop rows where tau1 and tau2 are not defined
+# Drop rows where tau1 and tau2 are not defined (inconsistent with the assumption of two channels)
 df2 = df2.dropna()
 
 unique_T_vals = np.unique(df2['T'].to_numpy())
 # In our remaining data there are eight unique T values
 
-# # Plot histograms of the different x values at each different T value
-# fig, axs = plt.subplots(2, int(np.ceil(0.5*len(unique_T_vals))))
-# for i in range(len(unique_T_vals)):
-#     T = unique_T_vals[i]
-#     df2_temp = df2[df2['T']==T] # Select out rows with the corresponding temperature
+# Plot histograms of the different x values at each different T value
+fig, axs = plt.subplots(2, int(np.ceil(0.5*len(unique_T_vals))))
+for i in range(len(unique_T_vals)):
+    T = unique_T_vals[i]
+    df2_temp = df2[df2['T']==T] # Select rows with the corresponding temperature
 
-#     x_av = df2_temp['x'].mean() # Average x at each T value
-#     x_av = round(x_av, 3)
-#     count = df2_temp.shape[0] # Number of valid data points at this T value
+    x_av = df2_temp['x'].mean() # Average x at each T value
+    x_av = round(x_av, 3)
+    count = df2_temp.shape[0] # Number of valid data points at this T value
 
-#     # Determine where in the subplot grid each plot will go (this is due to my weird indexing)
-#     subplot_row = int(i >= 0.5*len(unique_T_vals))
-#     subplot_col = i % int(np.floor(0.5*len(unique_T_vals)))
+    # Determine where in the subplot grid each plot will go (this is due to my weird indexing)
+    subplot_row = int(i >= 0.5*len(unique_T_vals))
+    subplot_col = i % int(np.floor(0.5*len(unique_T_vals)))
 
-#     # Plot a histogram of all x values at the given T value
-#     axs[subplot_row, subplot_col].hist(df2_temp['x'], bins=50, density=True)
-#     # Include temperature, average x, and count in the title of each subplot
-#     axs[subplot_row, subplot_col].set_title("$T = "+str(T)+r", \bar{x} = "+str(x_av)+", count = "+str(count)+"$")    
-#     if subplot_row==1:
-#         axs[subplot_row, subplot_col].set_xlabel("$x$")
+    # Plot a histogram of all x values at the given T value
+    axs[subplot_row, subplot_col].hist(df2_temp['x'], bins=50, density=True)
+    # Include temperature, average x, and count in the title of each subplot
+    axs[subplot_row, subplot_col].set_title("$T = "+str(T)+r", \bar{x} = "+str(x_av)+", count = "+str(count)+"$")    
+    if subplot_row==1:
+        axs[subplot_row, subplot_col].set_xlabel("$x$")
 
-# plt.show()
+plt.show()
 
 
 # Fit S - G data to a parabola - just one temperature value for now
@@ -114,9 +114,12 @@ df3 = df3[df3['T']==21.5] # Carry data points with specific T
 df3['s'] = df3['S']/(G0 * kB * (pi**2/9 - 2/3) * df3['DeltaT']**2 / df3['T'])
 df3 = df3.sort_values('G')
 
-
 popt, pcov = curve_fit(s_model, df3['G'], df3['s'])
+print(popt[0])
 
+# Show data with the curve resulting from the fit
 plt.scatter(df3['G'], df3['s'])
 plt.plot(df3['G'], s_model(df3['G'], popt[0]), color='orange')
+plt.xlabel("$G/G_0$")
+plt.ylabel("$S/G_0k_BT$")
 plt.show()
